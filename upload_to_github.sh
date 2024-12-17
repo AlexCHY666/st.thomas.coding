9ab501a6a6e650665a69074a85e62fc5140baa4e
Welpit69 <alexcheng0531@gmail.com>
2024-12-16 19:49:04 -0500

    Auto-update on Mon 16 Dec 2024 19:49:04 EST

--- upload_to_github.sh
+++ upload_to_github.sh
@@ -1,21 +1,13 @@
-#!/bin/bash
-
-# 定义仓库路径
 REPO_PATH="/Users/hello./Library/Mobile Documents/com~apple~CloudDocs/St. Thomas/Coding/St.Thomas-coding.git"
 
-# 切换到仓库路径
 cd "$REPO_PATH"
 
-# 拉取最新更改
 git pull origin main
 
-# 添加更改
 git add .
 
-# 提交更改
 git commit -m "Auto-update on $(date)"
 
-# 推送到 GitHub
 git push origin main
 
-echo "Changes have been pushed to GitHub!"
+echo "Pushed to GitHub"
