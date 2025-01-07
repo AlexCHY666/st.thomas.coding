a2d50706644719b8e8db87b485749c3ca17293d5
Welpit69 <alexcheng0531@gmail.com>
2025-01-07 12:30:32 -0500

    Auto-update on Tue  7 Jan 2025 12:30:32 EST

--- js/redirect_form.js
+++ js/redirect_form.js
@@ -1,10 +1,9 @@
 document.getElementById('googleForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent default form submission
+    event.preventDefault();
 
     const form = event.target;
     const formData = new FormData(form);
 
-    // Send the data to Google Forms using fetch
     fetch("https://docs.google.com/forms/d/e/1FAIpQLSeLUzCIfgjmIrQ5_WLAgCwliPWVqVraNd_mS3bSBL7XQs7KfQ/formResponse", {
         method: "POST",
         body: formData,
